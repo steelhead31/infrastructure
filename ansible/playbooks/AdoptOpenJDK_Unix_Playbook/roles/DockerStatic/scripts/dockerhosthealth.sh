e91e538b786e2a7a5eb5a95fed040c213a64e80c
Haroon Khel <hkhel@redhat.com>
2022-01-17 13:53:10 +0000

    simplify output

--- ansible/playbooks/AdoptOpenJDK_Unix_Playbook/roles/DockerStatic/scripts/dockerhosthealth.sh
+++ ansible/playbooks/AdoptOpenJDK_Unix_Playbook/roles/DockerStatic/scripts/dockerhosthealth.sh
@@ -5,25 +5,16 @@ containerIds=$(docker ps -q)
 
 echo "docker system df"
 docker system df
-
-reclaimableSpace=$(docker system df | grep Images | awk '{print $5}' | sed 's/GB//')
-if [[ $(echo "$reclaimableSpace > 10" | bc -l) ]]; then
-    echo "$reclaimableSpace of space can be reclaimed with docker system prune"
-fi
-
-echo "Disk space available on each docker container"
-df -h | grep overlay | head -n 1 | awk '{print $4}'
-echo "----------"
+echo ""
 
 for container in $containerIds
 do
 
-    echo "Container: $container"
-    workspaceSpace=$(docker exec $container sh -c "du -sh /home/jenkins/workspace")
-    echo $workspaceSpace
-    workspaceSpaceNumber=$(echo $workspaceSpace | awk '{print $1}')
-    if [[ $workspaceSpaceNumber -gt 3000000 ]]; then
-        echo "Container $container's workspace directory is using $workspaceSpaceNumber in space. Could use some clearing up"
+    workspaceSpace=$(docker exec $container sh -c "du -sh /home/jenkins/workspace" 2> /dev/null)
+    if [[ $? -eq 0 ]]; then
+        echo "Container: $container"
+        echo "$workspaceSpace"
+        echo ""
     fi
 
 done
