a829b6bedf1aa58b623e5bb086735796e90415ad
Haroon Khel <hkhel@redhat.com>
2022-01-17 12:22:15 +0000

    change df -k to -h

--- ansible/playbooks/AdoptOpenJDK_Unix_Playbook/roles/DockerStatic/scripts/dockerhosthealth.sh
+++ ansible/playbooks/AdoptOpenJDK_Unix_Playbook/roles/DockerStatic/scripts/dockerhosthealth.sh
@@ -8,7 +8,7 @@ docker system df
 
 reclaimableSpace=$(docker system df | grep Images | awk '{print $5}' | sed 's/GB//')
 if [[ $(echo "$reclaimableSpace > 10" | bc -l) ]]; then
-    echo "$reclaimableSpace GB of space can be reclaimed with docker system prune"
+    echo "$reclaimableSpace of space can be reclaimed with docker system prune"
 fi
 
 echo "Disk space available on each docker container"
@@ -19,7 +19,7 @@ for container in $containerIds
 do
 
     echo "Container: $container"
-    workspaceSpace=$(docker exec $container sh -c "du -sk /home/jenkins/workspace")
+    workspaceSpace=$(docker exec $container sh -c "du -sh /home/jenkins/workspace")
     echo $workspaceSpace
     workspaceSpaceNumber=$(echo $workspaceSpace | awk '{print $1}')
     if [[ $workspaceSpaceNumber -gt 3000000 ]]; then
